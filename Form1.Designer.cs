ab06de1b572e5b025e4f7727483efd09579ba951
vuong <vuong@MSI>
2023-07-24 10:25:26 +0700

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,295 @@
+﻿namespace WindowsFormsApp3
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.btthem = new System.Windows.Forms.Button();
+            this.btsua = new System.Windows.Forms.Button();
+            this.btxoa = new System.Windows.Forms.Button();
+            this.btluu = new System.Windows.Forms.Button();
+            this.btkluu = new System.Windows.Forms.Button();
+            this.btthoat = new System.Windows.Forms.Button();
+            this.listView1 = new System.Windows.Forms.ListView();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txtmasp = new System.Windows.Forms.TextBox();
+            this.txttensp = new System.Windows.Forms.TextBox();
+            this.dateTimePicker1 = new System.Windows.Forms.DateTimePicker();
+            this.backgroundWorker1 = new System.ComponentModel.BackgroundWorker();
+            this.listView2 = new System.Windows.Forms.ListView();
+            this.masp = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.label5 = new System.Windows.Forms.Label();
+            this.columnHeader1 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader2 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader3 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.domainUpDown1 = new System.Windows.Forms.DomainUpDown();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(220, 40);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "tìm";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // btthem
+            // 
+            this.btthem.Location = new System.Drawing.Point(90, 396);
+            this.btthem.Name = "btthem";
+            this.btthem.Size = new System.Drawing.Size(75, 23);
+            this.btthem.TabIndex = 2;
+            this.btthem.Text = "thêm ";
+            this.btthem.UseVisualStyleBackColor = true;
+            this.btthem.Click += new System.EventHandler(this.btthem_Click);
+            // 
+            // btsua
+            // 
+            this.btsua.Location = new System.Drawing.Point(274, 396);
+            this.btsua.Name = "btsua";
+            this.btsua.Size = new System.Drawing.Size(75, 23);
+            this.btsua.TabIndex = 3;
+            this.btsua.Text = "sửa";
+            this.btsua.UseVisualStyleBackColor = true;
+            // 
+            // btxoa
+            // 
+            this.btxoa.Location = new System.Drawing.Point(184, 396);
+            this.btxoa.Name = "btxoa";
+            this.btxoa.Size = new System.Drawing.Size(75, 23);
+            this.btxoa.TabIndex = 4;
+            this.btxoa.Text = "xóa ";
+            this.btxoa.UseVisualStyleBackColor = true;
+            // 
+            // btluu
+            // 
+            this.btluu.Location = new System.Drawing.Point(364, 396);
+            this.btluu.Name = "btluu";
+            this.btluu.Size = new System.Drawing.Size(75, 23);
+            this.btluu.TabIndex = 5;
+            this.btluu.Text = "lưu";
+            this.btluu.UseVisualStyleBackColor = true;
+            // 
+            // btkluu
+            // 
+            this.btkluu.Location = new System.Drawing.Point(462, 396);
+            this.btkluu.Name = "btkluu";
+            this.btkluu.Size = new System.Drawing.Size(75, 23);
+            this.btkluu.TabIndex = 6;
+            this.btkluu.Text = "k lưu";
+            this.btkluu.UseVisualStyleBackColor = true;
+            // 
+            // btthoat
+            // 
+            this.btthoat.Location = new System.Drawing.Point(552, 396);
+            this.btthoat.Name = "btthoat";
+            this.btthoat.Size = new System.Drawing.Size(75, 23);
+            this.btthoat.TabIndex = 7;
+            this.btthoat.Text = "thoát";
+            this.btthoat.UseVisualStyleBackColor = true;
+            // 
+            // listView1
+            // 
+            this.listView1.HideSelection = false;
+            this.listView1.Location = new System.Drawing.Point(73, 40);
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(121, 23);
+            this.listView1.TabIndex = 8;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(596, 104);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(44, 16);
+            this.label1.TabIndex = 9;
+            this.label1.Text = "mã sp";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(596, 164);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(43, 16);
+            this.label2.TabIndex = 10;
+            this.label2.Text = "tên sp";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(574, 213);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(73, 16);
+            this.label3.TabIndex = 11;
+            this.label3.Text = "ngày nhập ";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(596, 263);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(47, 16);
+            this.label4.TabIndex = 12;
+            this.label4.Text = "loại sp";
+            // 
+            // txtmasp
+            // 
+            this.txtmasp.Location = new System.Drawing.Point(646, 101);
+            this.txtmasp.Name = "txtmasp";
+            this.txtmasp.Size = new System.Drawing.Size(200, 22);
+            this.txtmasp.TabIndex = 13;
+            // 
+            // txttensp
+            // 
+            this.txttensp.Location = new System.Drawing.Point(646, 158);
+            this.txttensp.Name = "txttensp";
+            this.txttensp.Size = new System.Drawing.Size(200, 22);
+            this.txttensp.TabIndex = 14;
+            // 
+            // dateTimePicker1
+            // 
+            this.dateTimePicker1.Location = new System.Drawing.Point(646, 208);
+            this.dateTimePicker1.Name = "dateTimePicker1";
+            this.dateTimePicker1.Size = new System.Drawing.Size(200, 22);
+            this.dateTimePicker1.TabIndex = 16;
+            // 
+            // listView2
+            // 
+            this.listView2.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.masp,
+            this.columnHeader1,
+            this.columnHeader2,
+            this.columnHeader3});
+            this.listView2.HideSelection = false;
+            this.listView2.Location = new System.Drawing.Point(73, 104);
+            this.listView2.Name = "listView2";
+            this.listView2.Size = new System.Drawing.Size(495, 259);
+            this.listView2.TabIndex = 17;
+            this.listView2.UseCompatibleStateImageBehavior = false;
+            this.listView2.View = System.Windows.Forms.View.Details;
+            // 
+            // masp
+            // 
+            this.masp.Tag = "";
+            this.masp.Text = "mã sp";
+            this.masp.Width = 92;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(396, 29);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(155, 16);
+            this.label5.TabIndex = 18;
+            this.label5.Text = "DANH MỤC  SẢN PHẨM";
+            // 
+            // columnHeader1
+            // 
+            this.columnHeader1.Text = "tên sp";
+            this.columnHeader1.Width = 107;
+            // 
+            // columnHeader2
+            // 
+            this.columnHeader2.Text = "ngày nhập";
+            this.columnHeader2.Width = 121;
+            // 
+            // columnHeader3
+            // 
+            this.columnHeader3.Text = "loại sp";
+            this.columnHeader3.Width = 118;
+            // 
+            // domainUpDown1
+            // 
+            this.domainUpDown1.Cursor = System.Windows.Forms.Cursors.SizeNS;
+            this.domainUpDown1.Location = new System.Drawing.Point(649, 263);
+            this.domainUpDown1.Name = "domainUpDown1";
+            this.domainUpDown1.Size = new System.Drawing.Size(197, 22);
+            this.domainUpDown1.TabIndex = 19;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(877, 450);
+            this.Controls.Add(this.domainUpDown1);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.listView2);
+            this.Controls.Add(this.dateTimePicker1);
+            this.Controls.Add(this.txttensp);
+            this.Controls.Add(this.txtmasp);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.listView1);
+            this.Controls.Add(this.btthoat);
+            this.Controls.Add(this.btkluu);
+            this.Controls.Add(this.btluu);
+            this.Controls.Add(this.btxoa);
+            this.Controls.Add(this.btsua);
+            this.Controls.Add(this.btthem);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button btthem;
+        private System.Windows.Forms.Button btsua;
+        private System.Windows.Forms.Button btxoa;
+        private System.Windows.Forms.Button btluu;
+        private System.Windows.Forms.Button btkluu;
+        private System.Windows.Forms.Button btthoat;
+        private System.Windows.Forms.ListView listView1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox txtmasp;
+        private System.Windows.Forms.TextBox txttensp;
+        private System.Windows.Forms.DateTimePicker dateTimePicker1;
+        private System.ComponentModel.BackgroundWorker backgroundWorker1;
+        private System.Windows.Forms.ListView listView2;
+        private System.Windows.Forms.ColumnHeader masp;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.ColumnHeader columnHeader1;
+        private System.Windows.Forms.ColumnHeader columnHeader2;
+        private System.Windows.Forms.ColumnHeader columnHeader3;
+        private System.Windows.Forms.DomainUpDown domainUpDown1;
+    }
+}
+
